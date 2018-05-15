9f8d673a3ce5826349c61636a7c783d8d6ace8d3
cntchen <cntchen@gmail.com>
2018-05-15 15:21:47 +0800

    fix: preview inserted script error

--- server/preview-inserted-script.js
+++ server/preview-inserted-script.js
@@ -2,6 +2,7 @@ console.info('running preview inserted script');
 
 const receiveMessage = (event) => {
   if (!event.data) return;
+  if (!event.data.currentComponent) return;
 
   const componentId = event.data.currentComponent.id;
   const selector = `div[data-component-id="${componentId}"]`;
